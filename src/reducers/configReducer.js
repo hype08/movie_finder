80bd707f2b83b57d0ba3898612fe67f8454f8287
Henry <isomorphicdev@gmail.com>
2020-07-25 20:37:17 +0900

    add query-string aand svg + icon

--- src/reducers/configReducer.js
+++ src/reducers/configReducer.js
@@ -1,11 +1,17 @@
 import * as TYPES from '../actions/types';
 
-export default (state = {}, action) => {
+const INITIAL_STATE = {
+  staticCategories: ['Popular', 'Top Rated', 'Upcoming'],
+};
+
+export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case TYPES.GET_CONFIG:
       return { ...state, base: action.payload };
     case TYPES.GET_GENRES:
       return { ...state, ...action.payload };
+    case TYPES.SELECTED_MENU:
+      return { ...state, selected: action.payload };
     default:
       return state;
   }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import configReducer from './configReducer';
+import moviesReducer from './moviesReducer';
 
 export default combineReducers({
-  general: configReducer,
+  geral: configReducer,
+  movies: moviesReducer,
 });
