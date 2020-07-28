add03955585ccabe1e8e08e9914af89d2a97c137
Henry <isomorphicdev@gmail.com>
2020-07-29 00:47:44 +0900

    add helmet, utils/theme and move globalstyle to utils as well

--- src/index.js
+++ src/index.js
@@ -1,59 +1,31 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { Helmet } from 'react-helmet';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import styled, { createGlobalStyle } from 'styled-components';
-import { createStore, applyMiddleware, compose } from 'redux';
-import reduxThunk from 'redux-thunk';
 
-import App from './components/App';
-import reducers from './reducers';
+import store from './store';
+import { ThemeProvider } from 'styled-components';
+import theme from './utils/theme';
+import GlobalStyle from './utils/globals';
+import App from './containers/App';
 
-const GlobalStyle = createGlobalStyle`
-* {
-  margin: 0;
-  padding: 0;
-}
-
-*,
-*::before,
-*::after {
-  box-sizing: inherit;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
-
-html {
-  box-sizing: border-box;
-  font-size: 62.5%; //1rem = 10px
-  --color-primary-dark: #263238;
-  --color-primary: #37474f;
-  --color-primary-light: #546e7a;
-  --color-primary-lighter: #b0bec5;
-  --text-color: #fafafa;
-  --link-color: #444;
-  --border-color: rgba(176, 190, 197, 0.5);
-  --shadow-color: rgba(0, 0, 0, 0.2);
-  --shadow-color-dark: rgba(0, 0, 0, 0.25);
-}
-
-body {
-  @import url('https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700');
-  font-family: 'Montserrat', sans-serif;
-  font-weight: 400;
-  line-height: 1.6;
-}
-`;
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(
-  reducers,
-  composeEnhancers(applyMiddleware(reduxThunk))
-);
+import '../node_modules/react-modal-video/scss/modal-video.scss';
+import '../node_modules/slick-carousel/slick/slick.css';
+import '../node_modules/slick-carousel/slick/slick-theme.css';
 
 ReactDOM.render(
   <Provider store={store}>
-    <GlobalStyle />
-    <App />
+    <ThemeProvider theme={theme}>
+      <Fragment>
+        <Helmet>
+          <title>Movie Recs</title>
+          <meta name="description" content="Find Movie Recommendations" />
+          <link rel="canonical" href="https://moviedbproject.now.sh/" />
+        </Helmet>
+        <App />
+        <GlobalStyle />
+      </Fragment>
+    </ThemeProvider>
   </Provider>,
   document.querySelector('#root')
 );
