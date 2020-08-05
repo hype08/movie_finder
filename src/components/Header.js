50c83e2d82a6a1a96f59c1644cb840cc2ef5eaa9
Henry <isomorphicdev@gmail.com>
2020-08-05 21:47:33 +0900

    add types, header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,34 +1,45 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import styled from 'styled-components';
 
-import SearchBar from './SearchBar';
-
-const Wrapper = styled.div`
-  display: flex;
-  padding: 1rem;
-  align-items: center;
-  color: var(--color-primary);
-`;
-
 const Title = styled.h1`
-  font-size: 2.2rem;
-  font-weight: 700;
-  margin-right: auto;
+  font-size: ${props => (props.size === '2' ? '4rem' : '2.5rem')};
+  font-weight: 200;
+  line-height: ${props => (props.size === '2' ? '1.2' : '1')};
+  color: var(--color-primary-dark);
   letter-spacing: -0.5px;
+  text-transform: uppercase;
+  margin-bottom: 0.5rem;
+
+  @media ${props => props.theme.mediaQueries.medium} {
+    font-size: ${props => (props.size === '2' ? '2.7rem' : '2.2rem')};
+  }
+
+  @media ${props => props.theme.mediaQueries.small} {
+    font-size: ${props => (props.size === '2' ? '2.2rem' : '2rem')};
+  }
 `;
 
-const Header = ({ header }) => {
-  return (
-    <Wrapper>
-      <Title>{header}</Title>
-      <SearchBar />
-    </Wrapper>
-  );
-};
+const Subtitle = styled.h2`
+  text-transform: uppercase;
+  line-height: ${props => (props.size === '2' ? '1.5' : '1')};
+  color: var(--color-primary);
+  font-size: ${props => (props.size === '2' ? '1.7rem' : '1.2rem')};
+  font-weight: 700;
 
-const mapStateToProps = ({ general }) => {
-  return { header: general.header };
-};
+  @media ${props => props.theme.mediaQueries.medium} {
+    font-size: ${props => (props.size === '2' ? '1.3rem' : '1.1rem')};
+  }
+`;
 
-export default connect(mapStateToProps)(Header);
+const HeaderWrapper = styled.div`
+  margin-bottom: 2rem;
+`;
+
+const Header = ({ title, subtitle, size }) => (
+  <HeaderWrapper>
+    <Title size={size}>{title}</Title>
+    <Subtitle size={size}>{subtitle}</Subtitle>
+  </HeaderWrapper>
+);
+
+export default Header;
